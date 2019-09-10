f6d8785810294bd3a9229db952fd71bcadb4fc4b
Azamat Bekkhozha <azamatb@wimmersolutions.com>
2019-09-10 16:20:53 -0700

    Import minisom, creat clusters

--- som.py
+++ som.py
@@ -5,3 +5,12 @@ import pandas as pd
 dataset = pd.read_csv('Credit_Card_Applications.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
+
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+X = sc.fit_transform(X)
+
+from minisom import MiniSom
+som = MiniSom(x = 10, y = 10, input_len = 15, sigma = 1.0, learning_rate = 0.5)
+som.random_weights_init(X)
+som.train_random(data = X, num_iteration = 100)
