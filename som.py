d2ba6957b2b2837e636267f3d0d2f8bb5942b85e
Azamat Bekkhozha <azamatb@wimmersolutions.com>
2019-09-10 16:09:23 -0700

    Importing the dataset

--- som.py
+++ som.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import pandas as pd
+
+dataset = pd.read_csv('Credit_Card_Applications.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
