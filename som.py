70443118b960f1f8d0db8c49b0a8ab8c64929893
Azamat Bekkhozha <azamatb@wimmersolutions.com>
2019-09-12 16:15:48 -0700

    Get outliers and find frauds

--- som.py
+++ som.py
@@ -14,3 +14,40 @@ from minisom import MiniSom
 som = MiniSom(x = 10, y = 10, input_len = 15, sigma = 1.0, learning_rate = 0.5)
 som.random_weights_init(X)
 som.train_random(data = X, num_iteration = 100)
+
+from pylab import bone, pcolor, colorbar, plot, show
+bone()
+pcolor(som.distance_map().T)
+colorbar()
+markers = ['o', 's']
+colors = ['r', 'g']
+
+for i, x in enumerate(X):
+    w = som.winner(x)
+    plot(w[0] + 0.5,
+         w[1] + 0.5,
+         markers[y[i]],
+         markeredgecolor = colors[y[i]],
+         markerfacecolor = 'None',
+         markersize = 10,
+         markeredgewidth = 1)
+show()
+
+# Finding the frauds
+distance_map = som.distance_map().T
+outliers_x = []
+outliers_y = []
+frauds = []
+
+# threshold for frauds
+frauds_thr = 0.95
+
+for i in range(len(distance_map)):
+    for j in range(len(distance_map[i])):
+        if distance_map[i][j] >= frauds_thr:
+            outliers_x.append(i)
+            outliers_y.append(j)
+
+mappings = som.win_map(X)
+for i in range(len(outliers_x)):
+    frauds.append(mappings[outliers_x[i], outliers_y[i]])
